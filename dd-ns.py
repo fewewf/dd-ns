e5ad74d67bde5f9892deffc1d82fad813a815d3a
fewewf <32592150+fewewf@users.noreply.github.com>
2025-04-11 23:13:59 +0800

    Update dd-ns.py

--- dd-ns.py
+++ dd-ns.py
@@ -109,7 +109,7 @@ def main():
     try:
         log_existing_yx1_records()
 
-        top_ips = get_top_ips_from_csv("result.csv", start=3, count=3)
+        top_ips = get_ips_from_csv("result.csv", start=3, count=3)
         if not top_ips:
             raise Exception("CSV 中未读取到有效 IP")
 
